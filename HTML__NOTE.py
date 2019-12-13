067f40371f56bb6c86b7a5fd7549671c98e8599f
edagotti689 <53170802+edagotti689@users.noreply.github.com>
2019-12-13 22:21:58 +0530

    Add files via upload

---
+++ HTML__NOTE.py
@@ -0,0 +1,43 @@
+## Below is the HTML code
+<html>
+  <body>
+    <div id="pancakes">
+      <button type="button" name="pancake" value="Blueberry">Blueberry</button>
+      <button type="button" name="pancake" value="Banana">Banana</button>
+      <button type="button" name="pancake" value="Strawberry">Strawberry</button>
+    </div>
+  </body>
+</html>
+
+
+## HTML Element
+    1. An HTML element usually consists of a start tag and end tag, with the content inserted in between:
+
+    # Below is the HTML element 
+    <button type="button" name="pancake" value="Banana">Banana</button>
+
+## HTML Tags
+    1. HTML tags are element names surrounded by angle brackets:
+    2. HTML tags normally come in pairs like <p> and </p>
+    3. The first tag in a pair is the start tag, the second tag is the end tag
+    4. The end tag is written like the start tag, but with a forward slash inserted before the tag name
+    #     Example :- <tagname>content goes here...</tagname>
+
+
+
+## HTML Attributes
+    1. All HTML elements can have attributes
+    2. Attributes provide additional information about an element
+    3. Attributes are always specified in the start tag
+    4. Attributes usually come in name/value pairs like: name="value" 
+
+'''
+<button type="button" name="sriam" value="Banana">Banana</button>
+
+    type,name and value is the attributes in above Element 
+
+'''
+
+
+
+
